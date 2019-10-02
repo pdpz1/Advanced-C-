91692d99e2388becb08590168b3d51a4302cca6c
pdpz1 <56076310+pdpz1@users.noreply.github.com>
2019-10-02 13:55:00 +0100

    Add files via upload

---
+++ Worksheet1p1.cpp
@@ -0,0 +1,18 @@
+// Worksheet1p3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	string a;
+	cout << "Enter Name: ";
+	cin >> a;
+	cout << "\nHello " + a << "\nWelcome to CO7105";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
